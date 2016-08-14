4e1d3049bb8ab0fdf800d5bb81de17d522bbbf97
AlOrozco53 <alorozco.patriot53@gmail.com>
2016-08-14 18:07:44 -0500

    created constants and a function to encrypt words

--- Code/DataBase.py
+++ Code/DataBase.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import ..Constants.Constants
+
+def encrypt_pass(word):
+    """
+    Encrypts word by adding 3 units to each charachter's
+    ascii value. Allowed characters are specified in Readme.md
+    An error is raised if the word contains any character outside
+    the range [ASCII_INIT, ASCII_END] (see Constants/Constants.py)
+    :param word: word to encrypt
+    :returns an encrypted word:
+    """
+    # variable to store the encrypted word
+    encrypted = ''
+
+    for letter in word:
+        ascii_val = char(letter)
+        if ascii_val < ASCII_INIT or ascii_val > ASCII_END:
+            # the current letter is invalid!
+            raise IndexError('The current ascii value is out of the range'\
+                             + '[' + str(ASCII_INIT)\
+                             + str(ASCII_END) + ']')
+        else:
+            ascii_val -= ASCII_INIT
+            new_val = ((ascii_val + 3) % ASCII_LEN) + ASCII_INIT
+            encrypted += ord(new_val)
+
+    return encrypted
---
+++ Constants/Constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# index of the first character in the ascii
+# alphabet allowed to be in a username and/or password
+ASCII_INIT = 33
+
+# index of the last character in the ascii
+# alphabet allowed to be in a username and/or password
+ASCII_END = 126
+
+# length of the actual ascii subalphabet allowed to
+# build a username and/or password
+ASCII_LEN = 93
